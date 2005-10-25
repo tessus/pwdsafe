9753a66eef62648f5634d37f3d5ef37a88bb28fc
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2005-10-25 18:50:02 +0000

    add

---
+++ bldapp.bat
@@ -0,0 +1,64 @@
+@echo off
+
+rem $Id$
+
+rem Licensed Materials - Property of IBM
+rem
+rem Governed under the terms of the International
+rem License Agreement for Non-Warranted Sample Code.
+rem
+rem  (C) COPYRIGHT International Business Machines Corp. 1996 - 2002
+rem  All Rights Reserved.
+rem
+rem  US Government Users Restricted Rights - Use, duplication or
+rem  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+
+rem BATCH FILE: bldapp.bat
+rem Builds C applications on Windows
+rem Usage: bldapp prog_name [ db_name [ userid password ]]
+
+rem Default compiler is set to Microsoft Visual C++
+rem To use a different compiler, comment out 'set BLDCOMP=cl'
+rem and uncomment 'set BLDCOMP=icl' or 'set BLDCOMP=ecl'
+rem Microsoft C/C++ Compiler
+set BLDCOMP=cl
+
+rem Intel C++ Compiler for 32-bit applications
+rem set BLDCOMP=icl
+
+rem Intel C++ Compiler for Itanium 64-bit applications
+rem set BLDCOMP=ecl
+
+if exist "%1.sqx" goto embedded
+if exist "%1.sqc" goto embedded
+goto non_embedded
+
+:embedded
+rem Precompile and bind the program.
+rem Note: some .sqc/.sqx files contain no SQL but link in 
+rem utilemb.sqc/.sqx, so if you get this warning, ignore it:
+rem SQL0053W  No SQL statements were found in the program.
+call embprep %1 %2 %3 %4
+rem  Compile the program.
+if exist "%1.cxx" goto cpp_emb
+%BLDCOMP% -Zi -Od -c -W2 -DWIN32 %1.c utilemb.c
+goto link_embedded
+:cpp_emb
+%BLDCOMP% -Zi -Od -c -W2 -DWIN32 %1.cxx utilemb.cxx
+rem Link the program.
+:link_embedded
+link -debug -out:%1.exe %1.obj utilemb.obj db2api.lib
+goto exit
+
+:non_embedded
+rem  Compile the program.
+if exist "%1.cxx" goto cpp_non
+%BLDCOMP% -Zi -Od -c -W2 -DWIN32 %1.c utilapi.c
+goto link_non_embedded
+:cpp_non
+%BLDCOMP% -Zi -Od -c -W2 -DWIN32 %1.cxx utilapi.cxx
+rem Link the program.
+:link_non_embedded
+link -debug -out:%1.exe %1.obj utilapi.obj db2api.lib
+:exit
+@echo on
---
+++ embprep.bat
@@ -0,0 +1,56 @@
+@echo off
+
+rem $Id$
+
+rem Licensed Materials - Property of IBM
+rem
+rem Governed under the terms of the International
+rem License Agreement for Non-Warranted Sample Code.
+rem
+rem  (C) COPYRIGHT International Business Machines Corp. 1997 - 2003
+rem  All Rights Reserved.
+rem
+rem  US Government Users Restricted Rights - Use, duplication or
+rem  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+
+rem BATCH FILE: embprep.bat 
+rem Prep and binds a C/C++ or Micro Focus COBOL embedded SQL program on Windows
+rem Usage: embprep <prog_name> [ <db_name> [ <userid> <password> ]]
+
+rem Connect to a database.
+if "%1" == "" goto error
+if "%2" == "" goto case1
+if "%3" == "" goto case2
+if "%4" == "" goto error
+goto case3
+:case1
+   db2 connect to sample
+   goto prep_step
+:case2
+   db2 connect to %2
+   goto prep_step
+:case3
+   db2 connect to %2 user %3 using %4
+
+:prep_step
+if exist "%1.sqx" goto cpp
+if exist "%1.sqb" goto cob
+db2 prep %1.sqc bindfile
+if exist "utilemb.sqc" db2 prep utilemb.sqc
+goto bind_step
+:cpp
+db2 prep %1.sqx bindfile
+if exist "utilemb.sqx" db2 prep utilemb.sqx
+goto bind_step
+:cob
+db2 prep %1.sqb bindfile
+
+:bind_step
+db2 bind %1.bnd 
+db2 connect reset 
+
+goto exit
+:error
+echo Usage: embprep prog_name [ db_name [ userid password ]]
+:exit
+@echo on
