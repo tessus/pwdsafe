585760e1e6622e187b11528282d24c6351e837f2
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2008-12-26 06:38:38 +0000

    create bindfile names according to OS

--- embprep.bat
+++ embprep.bat
@@ -2,19 +2,8 @@
 
 rem $Id$
 
-rem Licensed Materials - Property of IBM
-rem
-rem Governed under the terms of the International
-rem License Agreement for Non-Warranted Sample Code.
-rem
-rem  (C) COPYRIGHT International Business Machines Corp. 1997 - 2003
-rem  All Rights Reserved.
-rem
-rem  US Government Users Restricted Rights - Use, duplication or
-rem  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
-
 rem BATCH FILE: embprep.bat 
-rem Prep and binds a C/C++ or Micro Focus COBOL embedded SQL program on Windows
+rem Prep and binds a C embedded SQL program on Windows
 rem Usage: embprep <prog_name> [ <db_name> [ <userid> <password> ]]
 
 rem Connect to a database.
@@ -33,21 +22,13 @@ goto case3
    db2 connect to %2 user %3 using %4
 
 :prep_step
-if exist "%1.sqx" goto cpp
-if exist "%1.sqb" goto cob
-db2 prep %1.sqc bindfile sqlerror continue validate run
-if exist "utilemb.sqc" db2 prep utilemb.sqc bindfile
+db2 prep %1.sqc bindfile using %1%W.bnd package using %1%W sqlerror continue validate run
+if exist "utilemb.sqc" db2 prep utilemb.sqc bindfile using utilembW.bnd package using utilembW
 goto bind_step
-:cpp
-db2 prep %1.sqx bindfile
-if exist "utilemb.sqx" db2 prep utilemb.sqx
-goto bind_step
-:cob
-db2 prep %1.sqb bindfile
 
 :bind_step
-db2 bind %1.bnd blocking all sqlerror continue validate run
-db2 bind utilemb.bnd
+db2 bind %1%W.bnd blocking all sqlerror continue validate run
+db2 bind utilembW.bnd
 db2 connect reset 
 
 goto exit
