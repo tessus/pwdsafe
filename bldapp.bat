20742ac6e071099af327df67f998f0a8c300a232
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2009-01-13 17:12:37 +0000

    changed compile options

--- bldapp.bat
+++ bldapp.bat
@@ -41,10 +41,10 @@ rem SQL0053W  No SQL statements were found in the program.
 call embprep %1 %2 %3 %4
 rem  Compile the program.
 if exist "%1.cxx" goto cpp_emb
-%BLDCOMP% /G7 -c -DWIN32 %1.c utilemb.c
+%BLDCOMP% /O2 /w -c -DWIN32 %1.c utilemb.c
 goto link_embedded
 :cpp_emb
-%BLDCOMP% /G7 -c -DWIN32 %1.cxx utilemb.cxx
+%BLDCOMP% -c -DWIN32 %1.cxx utilemb.cxx
 rem Link the program.
 :link_embedded
 link -out:%1.exe %1.obj utilemb.obj db2api.lib
@@ -53,10 +53,10 @@ goto exit
 :non_embedded
 rem  Compile the program.
 if exist "%1.cxx" goto cpp_non
-%BLDCOMP% /G7 -c -DWIN32 %1.c utilapi.c
+%BLDCOMP% -c -DWIN32 %1.c utilapi.c
 goto link_non_embedded
 :cpp_non
-%BLDCOMP% /G7 -c -DWIN32 %1.cxx utilapi.cxx
+%BLDCOMP% -c -DWIN32 %1.cxx utilapi.cxx
 rem Link the program.
 :link_non_embedded
 link -out:%1.exe %1.obj utilapi.obj db2api.lib
