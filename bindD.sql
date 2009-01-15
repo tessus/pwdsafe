e4722eda5ae4b4c82fe10bc961f44b410ca5a1a9
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2009-01-15 02:44:53 +0000

    added for MacOSX

---
+++ bindD.sql
@@ -0,0 +1,15 @@
+-- $Id$
+
+--CONNECT TO <database> user <USER>;
+
+BIND @pwdsafeD.lst BLOCKING ALL SQLERROR CONTINUE VALIDATE RUN;
+
+--CONNECT RESET;
+
+--TERMINATE;
+
+--<USER> needs SELECT, INSERT, UPDATE on PWDSAFE.PWDLOGIN
+--             SELECT, INSERT, UPDATE, DELETE on PWDSAFE.PWDLIST
+--             CREATE TABLE and BIND authority
+--             CREATE IN schema PWDSAFE
+
