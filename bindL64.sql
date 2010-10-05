18f0bde12bc71d82c7f743c03c7b2828d37b2eca
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2010-10-05 21:19:33 +0000

    added distinction for Linux packages on bitwidth

---
+++ bindL64.sql
@@ -0,0 +1,15 @@
+-- $Id$
+
+--CONNECT TO <database> user <USER>;
+
+BIND @pwdsafeL64.lst BLOCKING ALL SQLERROR CONTINUE VALIDATE RUN;
+
+--CONNECT RESET;
+
+--TERMINATE;
+
+--<USER> needs SELECT, INSERT, UPDATE on PWDSAFE.PWDLOGIN
+--             SELECT, INSERT, UPDATE, DELETE on PWDSAFE.PWDLIST
+--             CREATE TABLE and BIND authority
+--             CREATE IN schema PWDSAFE
+
