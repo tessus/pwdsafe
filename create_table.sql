94e3524f3c31acc421e457a7209b848fc12bab29
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2010-10-05 20:52:00 +0000

    adjusted the table definitions

--- create_table.sql
+++ create_table.sql
@@ -1,36 +1,36 @@
--- $Id$
-
---CONNECT TO <database>;
+CONNECT TO <database>;
 
 ------------------------------------------------
--- DDL Statements for table "PWDSAFE  "."PWDLOGIN"
+-- DDL Statements for Schemas
+------------------------------------------------
+
+CREATE SCHEMA "PWDSAFE " AUTHORIZATION "DB2INST1";
+COMMENT ON SCHEMA "PWDSAFE " IS 'Schema for PwdSafe Application';
+
+------------------------------------------------
+-- DDL Statements for table "PWDSAFE "."PWDLOGIN"
 ------------------------------------------------
  
- CREATE TABLE "PWDSAFE  "."PWDLOGIN"  (
+CREATE TABLE "PWDSAFE "."PWDLOGIN"  (
 		  "USR" VARCHAR(64) NOT NULL , 
-		  "PWD" VARCHAR(80) NOT NULL , 
-		  "ADMIN" INTEGER NOT NULL WITH DEFAULT 0 )   
+		  "PWD" VARCHAR(80) FOR BIT DATA NOT NULL , 
+		  "ADMIN" INTEGER NOT NULL WITH DEFAULT 0 , 
+		  "MENU" INTEGER NOT NULL WITH DEFAULT 1 )   
 		 IN "DATA4K" INDEX IN "IDX4K" ; 
-COMMENT ON TABLE "PWDSAFE  "."PWDLOGIN" IS 'Login Table for Password Application';
 
+COMMENT ON TABLE "PWDSAFE "."PWDLOGIN" IS 'Login Table for Password Application';
 
--- DDL Statements for primary key on Table "PWDSAFE  "."PWDLOGIN"
+-- DDL Statements for primary key on Table "PWDSAFE "."PWDLOGIN"
 
-ALTER TABLE "PWDSAFE  "."PWDLOGIN" 
+ALTER TABLE "PWDSAFE "."PWDLOGIN" 
 	ADD CONSTRAINT "PK_PWDLOGIN" PRIMARY KEY
 		("USR");
 
-
--- DDL Statements for aliases based on Table "PWDSAFE  "."PWDLOGIN"
-
-CREATE ALIAS "PWDSAFE "."PWDLOGIN" FOR "PWDSAFE  "."PWDLOGIN";
-
-
 ------------------------------------------------
--- DDL Statements for table "PWDSAFE  "."PWDLIST"
+-- DDL Statements for table "PWDSAFE "."PWDLIST"
 ------------------------------------------------
- 
- CREATE TABLE "PWDSAFE  "."PWDLIST"  (
+
+CREATE TABLE "PWDSAFE "."PWDLIST"  (
 		  "ACCOUNT" VARCHAR(64) NOT NULL , 
 		  "USERID" VARCHAR(64) NOT NULL , 
 		  "PASSWORD" VARCHAR(80) FOR BIT DATA , 
@@ -38,35 +38,47 @@ CREATE ALIAS "PWDSAFE "."PWDLOGIN" FOR "PWDSAFE  "."PWDLOGIN";
 		  "USR" VARCHAR(64) NOT NULL , 
 		  "TS" TIMESTAMP )   
 		 IN "DATA4K" INDEX IN "IDX4K" ; 
-COMMENT ON TABLE "PWDSAFE  "."PWDLIST" IS 'Pwd Table for Password Application';
 
+COMMENT ON TABLE "PWDSAFE "."PWDLIST" IS 'Pwd Table for Password Application';
 
--- DDL Statements for primary key on Table "PWDSAFE  "."PWDLIST"
+-- DDL Statements for primary key on Table "PWDSAFE "."PWDLIST"
 
-ALTER TABLE "PWDSAFE  "."PWDLIST" 
+ALTER TABLE "PWDSAFE "."PWDLIST" 
 	ADD CONSTRAINT "PK_PWDLIST" PRIMARY KEY
 		("ACCOUNT",
 		 "USR");
 
-
 --------------------------------------------
 -- Authorization Statements on Tables/Views 
 --------------------------------------------
-
- GRANT DELETE ON TABLE "PWDSAFE  "."PWDLIST" TO USER "DB2USER " ;
-
- GRANT INSERT ON TABLE "PWDSAFE  "."PWDLIST" TO USER "DB2USER " ;
-
- GRANT SELECT ON TABLE "PWDSAFE  "."PWDLIST" TO USER "DB2USER " ;
-
- GRANT UPDATE ON TABLE "PWDSAFE  "."PWDLIST" TO USER "DB2USER " ;
  
- GRANT SELECT ON TABLE "PWDSAFE  "."PWDLOGIN" TO USER "DB2USER " ;
+GRANT DELETE ON TABLE "PWDSAFE "."PWDLIST" TO USER "DB2USER " ;
+GRANT INSERT ON TABLE "PWDSAFE "."PWDLIST" TO USER "DB2USER " ;
+GRANT SELECT ON TABLE "PWDSAFE "."PWDLIST" TO USER "DB2USER " ;
+GRANT UPDATE ON TABLE "PWDSAFE "."PWDLIST" TO USER "DB2USER " ;
+GRANT INSERT ON TABLE "PWDSAFE "."PWDLOGIN" TO USER "DB2USER " ;
+GRANT SELECT ON TABLE "PWDSAFE "."PWDLOGIN" TO USER "DB2USER " ;
+GRANT UPDATE ON TABLE "PWDSAFE "."PWDLOGIN" TO USER "DB2USER " ;
 
- GRANT UPDATE ON TABLE "PWDSAFE  "."PWDLOGIN" TO USER "DB2USER " ;
+---------------------------------------
+-- Authorization Statements on Schemas 
+---------------------------------------
+ 
+GRANT CREATEIN ON SCHEMA "PWDSAFE " TO USER "DB2USER " ;
+GRANT DROPIN ON SCHEMA "PWDSAFE " TO USER "DB2USER " ;
+
+----------------------------------------
+-- Authorization Statements on Database 
+----------------------------------------
+ 
+GRANT CREATETAB ON DATABASE  TO USER "DB2USER " ;
+GRANT BINDADD ON DATABASE  TO USER "DB2USER " ;
+GRANT CONNECT ON DATABASE  TO USER "DB2USER " ;
+GRANT CONNECT ON DATABASE  TO GROUP "DB2USERS" ;
 
 COMMIT WORK;
 
---CONNECT RESET;
+CONNECT RESET;
 
---TERMINATE;
+TERMINATE;
+
